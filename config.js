8bcd91484fe482076269a9714fca678a66dd17b8
RYAN SINGH <71920588+ITZ-RYAN111@users.noreply.github.com>
2022-01-03 16:51:33 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+require("@firebase/firestore")
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkzHCmmA1wJ98_A--HGJJpLy0uLDWdbMM",
+  authDomain: "complaint-forum-77ada.firebaseapp.com",
+  projectId: "complaint-forum-77ada",
+  storageBucket: "complaint-forum-77ada.appspot.com",
+  messagingSenderId: "748641704821",
+  appId: "1:748641704821:web:58cab8d2ce99593776d13c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default fImageBackgroundBase.firestore()
+
